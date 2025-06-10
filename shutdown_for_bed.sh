c9061753f495d4b49536212dea03d17c9b7c5e8c
Ryan-AW <ryanawhittock@outlook.com>
2025-06-10 03:43:15 +0100

    added validation for hh:mm format time codes

--- shutdown_for_bed.sh
+++ shutdown_for_bed.sh
@@ -3,6 +3,18 @@ BEDTIME="10:00"
 DISARM_TIME="05:00"
 
 
+function validate_time_format() {
+	# returns 1 if the time code argument doesn't follow valid hh:mm format
+	if [[ $1 =~ ^([0-1][0-9]|2[0-3]):[0-5][0-9]$ ]]; then
+		return 0
+	fi
+	return 1
+}
+
+validate_time_format ${BEDTIME} ||
+	(echo "BEDTIME variable doesn't follow valid hh:mm format"; exit 1)
+validate_time_format ${DISARM_TIME} ||
+	(echo "DISARM_TIME variable doesn't follow valid hh:mm format"; exit 1)
 
 
 function fetch_datetime_utc() {
