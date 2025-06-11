bdb75e87659fb259875b495883cac8e6293bc224
Ryan-AW <ryanawhittock@outlook.com>
2025-06-11 19:50:25 +0100

    checks if a shutdown must be carried out immediatelly

--- shutdown_for_bed.sh
+++ shutdown_for_bed.sh
@@ -1,6 +1,6 @@
 # format "hh:mm"
-BEDTIME="10:00"
-DISARM_TIME="05:00"
+BEDTIME="10:34"
+DISARM_TIME="05:35"
 
 
 function validate_time_format() {
@@ -11,10 +11,14 @@ function validate_time_format() {
 	return 1
 }
 
-validate_time_format ${BEDTIME} ||
-	(echo "BEDTIME variable doesn't follow valid hh:mm format"; exit 1)
-validate_time_format ${DISARM_TIME} ||
-	(echo "DISARM_TIME variable doesn't follow valid hh:mm format"; exit 1)
+validate_time_format ${BEDTIME} || {
+	echo "BEDTIME variable doesn't follow valid hh:mm format"
+	exit 1
+}
+validate_time_format ${DISARM_TIME} || {
+	echo "DISARM_TIME variable doesn't follow valid hh:mm format"
+	exit 1
+}
 
 
 function fetch_time_utc() {
@@ -26,9 +30,60 @@ function fetch_time_utc() {
 	rm ${_output_file}
 
 	utc_time=${_datetime:11:5}
-	validate_time_format ${utc_time} ||
-		(echo 'something went wrong when fetching or decoding time from API'; exit 1)
+	validate_time_format ${utc_time} || {
+		echo 'something went wrong when fetching or decoding time from API'
+		exit 1
+	}
 }
+
+
+function trigger_shutdown() {
+	shutdown now
+}
+
+
+function check_if_shutdown_time() {
+	function convert_to_minutes() {
+	    local time=$1
+	    local hours=${time:0:2}
+	    local minutes=${time:3:2}
+	    echo $((10#$hours * 60 + 10#$minutes))
+	}
+
+	validate_time_format ${1} || {
+		echo "START_TIME doesn't follow valid hh:mm format"
+		exit 1
+	}
+	validate_time_format ${2} || {
+		echo "CUR_TIME doesn't follow valid hh:mm format"
+		exit 1
+	}
+	validate_time_format ${3} || {
+		echo "END_TIME doesn't follow valid hh:mm format"
+		exit 1
+	}
+
+	start_time=$1
+	current_time=$2
+	end_time=$3
+
+	start_total_minutes=$(convert_to_minutes "$start_time")
+	cur_total_minutes=$(convert_to_minutes "$current_time")
+	end_total_minutes=$(convert_to_minutes "$end_time")
+
+	if [[ $end_total_minutes -ge $start_total_minutes ]]; then
+		if [[ $cur_total_minutes -ge $start_total_minutes && $cur_total_minutes -lt $end_total_minutes ]]; then
+			trigger_shutdown
+		elif [[ $cur_total_minutes -eq $start_total_minutes && $cur_total_minutes -eq $end_total_minutes ]]; then
+			trigger_shutdown
+		fi
+	elif [[ $cur_total_minutes -ge $start_total_minutes || $cur_total_minutes -lt $end_total_minutes ]]; then
+		trigger_shutdown
+	fi
+}
+
 
 fetch_time_utc
 echo "time UTC: $utc_time"
+
+check_if_shutdown_time ${BEDTIME} ${utc_time} ${DISARM_TIME}
